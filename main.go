610f5663538f45e0961520947d61e1d3424e049d
Jithendra Kumar <ujk222@gmail.com>
2025-08-05 01:22:45 +0530

    changes new route create

--- main.go
+++ main.go
@@ -13,5 +13,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome Jithendra Kumar",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
